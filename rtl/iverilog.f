6001c77a5b8c26624be7bbaced01bf10ec1be849
adityabedekar17 <abedekar@ucsc.edu>
2025-01-25 18:20:11 -0800

    merge resolved

---
+++ rtl/iverilog.f
@@ -0,0 +1,5 @@
+/home/ad17/293/fpga-alu-uart/third_party/alexforencich_uart/rtl/uart_tx.v
+/home/ad17/293/fpga-alu-uart/third_party/alexforencich_uart/rtl/uart_rx.v
+/home/ad17/293/fpga-alu-uart/rtl/uart_echo.sv
+home/ad17/293/fpga-alu-uart/rtl/top.sv
+/home/ad17/293/oss-cad-suite/share/yosys/ice40/cells_sim.v
